212b2d08f8d80e1b3782b913f480992f2ce345cd
Ryan Emerson <remerson@redhat.com>
2019-08-09 17:40:19 +0100

    Basics of JGroups config in place

--- config-generator/src/main/groovy/Main.groovy
+++ config-generator/src/main/groovy/Main.groovy
@@ -1,20 +1,13 @@
 import groovy.text.XmlTemplateEngine
 import org.yaml.snakeyaml.Yaml
 
-Yaml parser = new Yaml()
-InputStream inputStream = getClass()
+def inputStream = getClass()
         .getClassLoader()
-        .getResourceAsStream("identities.yaml");
-Map example = parser.load(inputStream)
-example.each { println "Key=$it.key, Val=$it.value" }
-
-def credentials = example.get("credentials")
-def certificates = example.get("certificates")
-def oauth = example.get("oath")
+        .getResourceAsStream("config.yaml");
+def ispnConfig = new Yaml().load(inputStream)
 
 def xmlEngine = new XmlTemplateEngine()
-def xml = new File(getClass().getResource('example.xml').toURI()).text
-def xmlBinding = [sslEnabled: false]
-def xmlOutput = xmlEngine.createTemplate(xml).make(xmlBinding).toString()
+def xml = new File(getClass().getResource('template.xml').toURI()).text
+def xmlOutput = xmlEngine.createTemplate(xml).make(ispnConfig).toString()
 
 print xmlOutput
