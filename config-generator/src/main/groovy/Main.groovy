aaa637be87466df6180b6b94eabc5ef90f6cd075
Ryan Emerson <remerson@redhat.com>
2019-08-13 15:01:48 +0100

    Digest support added

--- config-generator/src/main/groovy/Main.groovy
+++ config-generator/src/main/groovy/Main.groovy
@@ -2,33 +2,34 @@ import groovy.text.XmlTemplateEngine
 import org.codehaus.groovy.runtime.ProcessGroovyMethods
 import org.yaml.snakeyaml.Yaml
 
-def static printErrorAndExit(String error) {
-    println(error)
-    System.exit(1)
+import java.security.MessageDigest
+
+static void printErrorAndExit(String error) {
+    println error
+    System.exit 1
 }
 
 static String addSeparator(String path) {
     return path.endsWith(File.separator) ? path : "${path}${File.separator}"
 }
 
-static def exec(String cmd) {
-    ProcessGroovyMethods.consumeProcessErrorStream(cmd.execute(), System.err)
+static void exec(String cmd) {
+    ProcessGroovyMethods.consumeProcessErrorStream cmd.execute(), System.err
 }
 
-if (args.length != 3) printErrorAndExit 'Usage: CONFIG_YAML IDENTITIES_YAML OUTPUT_DIR'
+static void processTemplate(String templateName, String dest, Map binding) {
+    String template = Main.getResourceAsStream(templateName).text
+    new XmlTemplateEngine()
+            .createTemplate(template)
+            .make(binding)
+            .writeTo(new File(dest).newWriter())
+}
 
-Map configYaml = new Yaml().load(new File(args[0]).newInputStream())
-Map identitiesYaml = new Yaml().load(new File(args[1]).newInputStream())
-def outputDir = addSeparator args[2]
+static void createKeystore(ks, String outputDir) {
+    if (ks.crtPath == null) return
 
-// Add additional params to config map
-configYaml.bindAddress = InetAddress.localHost.hostAddress
-
-// Create Keystore if required
-def ks = configYaml.keystore
-if (ks.crtPath != null) {
     def ksRoot = ks.path == null ? new File("${outputDir}keystores") : new File(ks.path).parentFile
-    ksRoot.mkdirs();
+    ksRoot.mkdirs()
     ksRoot = addSeparator ksRoot.getAbsolutePath()
     String crtSrc = addSeparator((String) ks.crtPath)
     String ksPkcs = "${ksRoot}keystore.pkcs12"
@@ -42,19 +43,50 @@ if (ks.crtPath != null) {
     exec "keytool -importkeystore -noprompt -srckeystore ${ksPkcs} -srcstoretype pkcs12 -srcstorepass ${ks.password} -destkeystore ${ks.path} -deststoretype pkcs12 -storepass ${ks.password}"
 }
 
+static void processCredentials(credentials, String outputDir, realm = "default") {
+    if (!credentials) return
+
+    def (users, groups) = [new Properties(), new Properties()]
+    credentials.each { c ->
+        MessageDigest md5 = MessageDigest.getInstance "MD5"
+        byte[] hashed = md5.digest "${c.username}:${realm}:${c.password}".getBytes("UTF-8")
+        users.put c.username, hashed.encodeHex().toString()
+        groups.put c.username, c.roles.join(",")
+    }
+    users.store new File("${outputDir}users.properties").newWriter(), "\$REALM_NAME=${realm}\$"
+    groups.store new File("${outputDir}groups.properties").newWriter(), null
+}
+
+private static String toHex(byte[] bytes) {
+    StringBuilder sb = new StringBuilder();
+    for (byte b : bytes) {
+        sb.append(String.format("%02x", b));
+    }
+    return sb.toString();
+}
+
+static void processIdentities(Map identities, String outputDir) {
+    processCredentials identities.credentials, outputDir
+}
+
+if (args.length != 3) printErrorAndExit 'Usage: CONFIG_YAML IDENTITIES_YAML OUTPUT_DIR'
+
+Map configYaml = new Yaml().load(new File(args[0]).newInputStream())
+Map identitiesYaml = new Yaml().load(new File(args[1]).newInputStream())
+def outputDir = addSeparator args[2]
+
+// Add additional params to config map
+configYaml.bindAddress = InetAddress.localHost.hostAddress
+
+// Create Keystore if required
+createKeystore configYaml.keystore, outputDir
+
 // Generate Infinispan configuration
-def ispnTemplate = getClass().getResourceAsStream('infinispan.xml').text
-def ispnConfig = new File("${outputDir}infinispan.xml")
-new XmlTemplateEngine()
-        .createTemplate(ispnTemplate)
-        .make(configYaml)
-        .writeTo(ispnConfig.newWriter())
+processTemplate 'infinispan.xml', "${outputDir}infinispan.xml", configYaml
 
 // Generate JGroups stack file
 def transport = configYaml.jgroups.transport
-def jgroupsTemplate = getClass().getResourceAsStream("jgroups-${transport}.xml").text
-def jgroupsConfig = new File("${outputDir}jgroups-${transport}.xml")
-new XmlTemplateEngine()
-        .createTemplate(jgroupsTemplate)
-        .make(configYaml)
-        .writeTo(jgroupsConfig.newWriter())
+processTemplate "jgroups-${transport}.xml", "${outputDir}jgroups-${transport}.xml", configYaml
+
+// Process Identities
+processIdentities identitiesYaml, outputDir
