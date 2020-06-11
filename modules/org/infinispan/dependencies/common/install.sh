912104576eeb69876803ddbc501ffc3b2ddc0aa9
Ryan Emerson <remerson@redhat.com>
2020-06-11 13:23:12 +0100

    UBI 8.2 image with openjdk-devel package

--- modules/org/infinispan/dependencies/common/install.sh
+++ modules/org/infinispan/dependencies/common/install.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Create symlink between bsdtar and tar to enable `oc copy`
-ln -s $(command -v bsdtar) /usr/bin/tar
+# ln -s $(command -v bsdtar) /usr/bin/tar
--- modules/org/infinispan/dependencies/infinispan/install.sh
+++ modules/org/infinispan/dependencies/infinispan/install.sh
@@ -2,4 +2,4 @@
 set -e
 
 # Remove dnf and rpm files as package manager not required at runtime
-rm -rf /var/lib/rpm /var/lib/dnf
+#rm -rf /var/lib/rpm /var/lib/dnf
